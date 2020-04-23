b252f204207d1ded91d4d8c5e6cabf6a8df23da9
RamenHoang <anhnguyenhoang321@gmail.com>
2020-04-23 16:58:44 +0700

    2st

--- src/config/session.js
+++ src/config/session.js
@@ -10,7 +10,7 @@ let dbPort = dbVals.dbPort;
 let dbName = dbVals.dbName;
 
 let sessionStore = new store({
-  url: `mongodb+srv://ramen-manager:Hakuouki123@cluster-vid-d9htd.mongodb.net/test?retryWrites=true&w=majority`,
+  url: 'mongodb+srv://ramen-manager:Hakuouki123@cluster-vid-d9htd.mongodb.net/test?retryWrites=true&w=majority',
   autoReconnect: true
 });
 
